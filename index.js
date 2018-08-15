a82f4640383a8f834edb63566ffcf0aed798e331
OmranAbazid <omran.1994@gmail.com>
2018-08-15 16:43:14 +0200

    initial bot setup

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ const {
 
 const Botkit = require("botkit");
 
+const DEFUALT_USER = {
+  type: "direct_message",
+  user: "UC8PT88BE",
+  channel: "DC8T86G1H"
+};
+
+var controller = Botkit.slackbot({
+  clientId: "418375872038.417185195285",
+  clientSecret: "3c040c065f1e59fce74be2ed3d899d5b",
+  scopes: ["bot"]
+});
+
+var bot = controller
+  .spawn({
+    token: "xoxb-418375872038-416341547520-Sa9qPMwFs8R8ZyBBQISLu1dP"
+  })
+  .startRTM();
+
 const config = {
-  baseUrl: "http://localhost:8080/engine-rest",
-  use: logger
+  baseUrl: "http://localhost:8080/engine-rest"
+  // use: logger
 };
 
 const customOption = {
@@ -22,31 +40,30 @@ const customOption = {
   ]
 };
 
+controller.hears(
+  ["hello", "hi"],
+  "direct_message,direct_mention,mention",
+  function(bot, message) {
+    console.log(message);
+    bot.reply(message, "hello");
+  }
+);
+
 // create a Client instance with custom configuration
 const client = new Client(config);
 
 // create a handler for the task
 const handler = async ({ task, taskService }) => {
   // get task variable;
-  //   const {
-  //     creditor,
-  //     invoiceCategory,
-  //     amount,
-  //     invoiceNumber,
-  //     invoiceDocumentUpload
-  //   } = task.variables.getAll();
+  // const {
+  //   creditor,
+  //   invoiceCategory,
+  //   amount,
+  //   invoiceNumber,
+  //   invoiceDocumentUpload
+  // } = task.variables.getAll();
 
-  //   var controller = Botkit.anywhere(configuration);
-
-  //   controller.hears("hello", "direct_message", function(bot, message) {
-  //     bot.reply(message, "Hello yourself!");
-  //   });
-  // 3
-  //   // TODO: set process variables
-  //   // const creditor = 'foo';
-  //   const processVariables = new Variables()
-  //     .set("creditor", creditor)
-  //     .set("bar", new Date());
+  bot.reply(DEFUALT_USER, "welcome");
 
   // complete the task
   try {
